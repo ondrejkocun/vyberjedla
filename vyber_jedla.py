47ef97e311a55ba05f6af13b823898449e4b92a9
ondrejkocun <113329606+ondrejkocun@users.noreply.github.com>
2022-09-27 19:40:24 +0200

    Update vyber_jedla.py

--- vyber_jedla.py
+++ vyber_jedla.py
@@ -1,30 +1,29 @@
-import tkinter
+import tkinter  #nastavenie platna
 canvas=tkinter.Canvas(width=500,height=200)
 canvas.pack()
-farby=['green','red','blue','orange']
-farby1=['z','c','m','o']
-subor=open('vyberjedla.txt','w')
-x=30
-for i in range(4):
+farby=['green','red','blue','orange']   #deklarovanie zoznamu
+farby1=['z','c','m','o']    #deklarovanie zoznamu
+subor=open('vyberjedla.txt','w')    #otvorenie textaku
+x=30    #nastavenie premmenej na hodnotu 30
+for i in range(4):  #cyklus na kreslenie stvorcov
     canvas.create_rectangle(x,50,x+100,150,fill=farby[i])
-    x+=+110
+    x+=+110 #zvacsenie premmenej 
  
-canvas.create_text(250,25,text='Výber jedla',font='Arial 25 ',fill='red')
+canvas.create_text(250,25,text='Výber jedla',font='Arial 25 ',fill='red')   #vypisanie textu 
 
-def klik(event):
-    subor=open('vyberjedla.txt','a')
-    xy=30
-    x=event.x
-    y=event.y
-    text=entry1.get()
-    print(x,y)
-    if text!='':
-        for i in range(4):
+def klik(event):    #funkcia spustena kliknutim
+    subor=open('vyberjedla.txt','a')    #otvorenie suboru na pridavanie
+    xy=30   #nastavenie premmenej
+    x=event.x   #nastaevnie premmenej na aktualnu poziciu mysi
+    y=event.y   #nastavenie premmenej na aktualnu poziciiu mysi
+    text=entry1.get()   #nastavenie premmenej na vysledok z entry
+    if text!='':    #podmienka ktora povoli zapisovanie az vtedy ak nieje prazdne entry
+        for i in range(4):  #cyklus na zistenie kde sa nachadza mys
             if y>50 and y<150 and x>xy and x<xy+120:
-                text+=' '+farby1[i]+'\n'
-                subor.write(text)
-            xy+=110
-        subor.close()
-entry1=tkinter.Entry()
-entry1.pack()
-canvas.bind('<Button-1>',klik)
+                text+=' '+farby1[i]+'\n'    #pridanie farbu stvorca na ktorej bola mys do premmenej
+                subor.write(text)   #zapis premmenej do textaku
+            xy+=110 #zvacsenie premennej
+        subor.close()   #zavretie suboru
+entry1=tkinter.Entry()  #nastavenie entry
+entry1.pack() 
+canvas.bind('<Button-1>',klik) #nastavenie bindu 
